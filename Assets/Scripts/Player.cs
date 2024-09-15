7fb83ac9db123abf3c5c60efb71346cc4d293510
alex <inrag3x@gmail.com>
2024-09-15 14:23:43 +0300

    add: lesson 4

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,10 +3,12 @@ using System.Linq;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D))]
+[RequireComponent(typeof(SpriteRenderer), typeof(Animator))]
 public class Player : MonoBehaviour
 {
     private const string Horizontal = "Horizontal";
     private const string Ground = "Ground";
+    private static readonly int State = Animator.StringToHash("State");
 
     [SerializeField] private float speed;
     [SerializeField] private float jumpForce;
@@ -16,11 +18,15 @@ public class Player : MonoBehaviour
     private bool isGrounded;
     private float xMax;
     private Rigidbody2D rigidbody2d;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
 
 
     private void Awake()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
         //Вообще не должно быть тут)
         var camera = Camera.main!;
         xMax = camera.orthographicSize * camera.aspect;
@@ -28,18 +34,42 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-        //И этого тоже!
-        float inputAxis = Input.GetAxis(Horizontal);
+        float horizontal = Input.GetAxis(Horizontal); //И этого тоже!
+        UpdateVelocity(horizontal);
+        UpdateAnimaton(horizontal);
+        UpdateRotation(horizontal);
+    }
+
+    private void UpdateAnimaton(float horizontal)
+    {
+        if (isGrounded)
+        {
+            animator.SetInteger(State, horizontal != 0 ? 1 : 0);
+        }
+    }
+
+    private void UpdateRotation(float horizontal)
+    {
+        spriteRenderer.flipX = horizontal switch
+        {
+            < 0 => false,
+            > 0 => true,
+            _ => spriteRenderer.flipX
+        };
+    }
+
+    private void UpdateVelocity(float horizontal)
+    {
         velocity = rigidbody2d.velocity;
 
-        if (transform.position.x < -xMax + 0.5f && inputAxis <= 0 ||
-            transform.position.x > xMax - 0.5f && inputAxis >= 0)
+        if (transform.position.x < -xMax + 0.5f && horizontal <= 0 ||
+            transform.position.x > xMax - 0.5f && horizontal >= 0)
         {
             velocity.x = 0;
         }
         else
         {
-            velocity.x = inputAxis * speed;
+            velocity.x = horizontal * speed;
         }
 
         rigidbody2d.velocity = velocity;
@@ -72,6 +102,7 @@ public class Player : MonoBehaviour
 
     private void Jump()
     {
+        animator.SetInteger(State, 2);
         rigidbody2d.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
     }
 }
