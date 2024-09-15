9f823d845ad6f11d1c99612045ab94a18bc637c8
alex <inrag3x@gmail.com>
2024-09-15 15:33:39 +0300

    add: lesson 7

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,17 +1,83 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using System.Linq;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer))]
 public class Enemy : MonoBehaviour
 {
-    private void Start()
+    private const string Player = "Player";
+    [SerializeField] private float speed;
+    private Rigidbody2D rigidbody2d;
+    private Vector2 direction = Vector2.left;
+    private SpriteRenderer spriteRenderer;
+    private Vector2 velocity;
+    private float xMax;
+
+    private void Awake()
     {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        var camera = Camera.main!;
+        xMax = camera.orthographicSize * camera.aspect;
     }
 
-    private void Update()
+    private void Start()
     {
+        rigidbody2d.velocity = direction * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        UpdateRotation(direction.x);
+        UpdateVelocity();
+    }
+
+    private void UpdateVelocity()
+    {
+        if (transform.position.x < -xMax + 0.5f ||
+            transform.position.x > xMax - 0.5f)
+        {
+            ChangeDirection(true);
+        }
+    }
+
+
+    private void UpdateRotation(float horizontal)
+    {
+        spriteRenderer.flipX = horizontal switch
+        {
+            < 0 => false,
+            > 0 => true,
+            _ => spriteRenderer.flipX
+        };
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!collision.gameObject.CompareTag(Player))
+        {
+            ChangeDirection(!collision.contacts.All(contact => contact.point.y < transform.position.y));
+            return;
+        }
+        bool dead = collision.contacts.All(contact => contact.point.y > transform.position.y);
+        if (dead)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    private void ChangeDirection(bool makeChange)
+    {
+        if (!makeChange)
+            return;
+        direction = -direction;
+        var velocity = rigidbody2d.velocity;
+        velocity.x = direction.x * speed;
+        rigidbody2d.velocity = velocity;
     }
 }
